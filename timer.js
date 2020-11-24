2ba2127b6fc18cc61efd1985c61450e5472a04e6
TusharPatil010010 <tusharp010010@gmail.com>
2020-11-24 06:44:05 +0530

    Added : added functionality to check the async nature of javascript

--- timer.js
+++ timer.js
@@ -1,1 +1,13 @@
-//Initialising program
+function showTime() {
+    const date = new Date();
+    return "Hrs: " + date.getHours + "Minutes: " + date.getMinutes +
+            "Seconds: " +  date.getSeconds;
+}
+
+function showSessionExpire() {
+    console.log("Activity-B your session is expired " + showTime);
+}
+
+console.log("Activity-A : Trigerring Activity-B at " + showTime());
+setTimeout(showSessionExpire, 5000);
+console.log("Activity-A: Trigerred Activity-B at " + showTime() + " will execute after 5 seconds");
